2f529c34e754ac744ab4cfcc88df71e8ecae5434
Andy Bettisworth <andy.bettisworth@gmail.com>
2021-10-03 14:45:37 -0500

    cluster_version

--- inputs.tf
+++ inputs.tf
@@ -15,6 +15,12 @@ variable "region" {
   default     = "us-west-2"
 }
 
+variable "cluster_version" {
+  description = "Kubernetes version to use for the EKS cluster."
+  type        = string
+  default     = "1.21"
+}
+
 variable "cluster_name" {
   description = "Name of the EKS cluster. Also used as a prefix in names of related resources."
   type        = string

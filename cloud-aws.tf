4a342702b57287b8b67da30b6e20349ca9f90372
Andy Bettisworth <andy.bettisworth@gmail.com>
2021-10-05 23:14:14 -0500

    pass var thru

--- cloud-aws.tf
+++ cloud-aws.tf
@@ -2,7 +2,21 @@ module "cloud_aws" {
   source = "./modules/cloud-aws"
   count  = var.cloud == "aws" ? 1 : 0
 
-  cluster_version = var.cluster_version
+  cluster_version               = var.cluster_version
+  cluster_enabled_log_types     = var.cluster_enabled_log_types
+  cluster_log_retention_in_days = var.cluster_log_retention_in_days
+  cluster_log_kms_key_id        = var.cluster_log_kms_key_id
+
+  cluster_create_endpoint_private_access_sg_rule = var.cluster_create_endpoint_private_access_sg_rule
+  cluster_endpoint_private_access_cidrs          = var.cluster_endpoint_private_access_cidrs
+  cluster_endpoint_private_access_sg             = var.cluster_endpoint_private_access_sg
+  cluster_endpoint_private_access                = var.cluster_endpoint_private_access
+
+  cluster_endpoint_public_access_cidrs = var.cluster_endpoint_public_access_cidrs
+  cluster_endpoint_public_access       = var.cluster_endpoint_public_access
+
+  cluster_egress_cidrs = var.cluster_egress_cidrs
+  workers_egress_cidrs = var.workers_egress_cidrs
 
   worker_groups                 = var.worker_groups
   workers_group_defaults        = var.workers_group_defaults

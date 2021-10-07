12b730b4bb6b3b93c157a707eebfd707efcfa4e2
Andy Bettisworth <andy.bettisworth@gmail.com>
2021-10-07 04:53:15 -0500

    pass list

--- modules/cloud-aws/aws_auth.tf
+++ modules/cloud-aws/aws_auth.tf
@@ -49,7 +49,7 @@ resource "kubernetes_config_map" "aws_auth" {
     # should be able to authenticate.
     mapAccounts = yamlencode(concat(
       # AWS Account ID where this config is deployed.
-      [join("", data.aws_caller_identity.current.account_id)],
+      [data.aws_caller_identity.current.account_id],
       var.map_aws_accounts,
     ))
   }

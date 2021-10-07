196739967c351c35a3d547d4476cf0bd2234e03d
Andy Bettisworth <andy.bettisworth@gmail.com>
2021-10-06 23:10:57 -0500

    rm depends_on

--- modules/cloud-aws/outputs.tf
+++ modules/cloud-aws/outputs.tf
@@ -1,35 +1,29 @@
 output "cluster_id" {
   description = "The ID of the EKS cluster."
   value       = local.cluster_id
-  depends_on  = [data.http.wait_for_cluster]
 }
 
 output "cluster_arn" {
   description = "The Amazon Resource Name (ARN) of the cluster."
   value       = local.cluster_arn
-  depends_on  = [data.http.wait_for_cluster]
 }
 
 output "cluster_endpoint" {
   description = "Endpoint of the cluster."
   value       = local.cluster_endpoint
-  depends_on  = [data.http.wait_for_cluster]
 }
 
 output "cluster_version" {
   description = "The Kubernetes server version for the EKS cluster."
   value       = element(concat(aws_eks_cluster.main[*].version, [""]), 0)
-  depends_on  = [data.http.wait_for_cluster]
 }
 
 output "cluster_certificate_authority_data" {
   description = "Base64 encoded certificate data required to communicate with your cluster."
   value       = local.cluster_auth_base64
-  depends_on  = [data.http.wait_for_cluster]
 }
 
 output "kubeconfig" {
   description = "kubectl config file contents for this EKS cluster."
   value       = local.kubeconfig
-  depends_on  = [data.http.wait_for_cluster]
 }

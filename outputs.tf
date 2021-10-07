61e8be0163c4c34aa0c81f0354442c1f42dd7bb3
Andy Bettisworth <andy.bettisworth@gmail.com>
2021-10-06 23:19:40 -0500

    rm

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "cluster_id" {
   description = "The ID of the EKS cluster."
-  value       = module.cloud_aws.cluster_id
+  value       = module.cloud_aws.0.cluster_id
 }
 
 output "cluster_arn" {
   description = "The Amazon Resource Name (ARN) of the cluster."
-  value       = module.cloud_aws.cluster_arn
+  value       = module.cloud_aws.0.cluster_arn
 }
 
 output "cluster_endpoint" {
   description = "Endpoint of the cluster."
-  value       = module.cloud_aws.cluster_endpoint
+  value       = module.cloud_aws.0.cluster_endpoint
 }
 
 output "cluster_version" {
   description = "The Kubernetes server version for the EKS cluster."
-  value       = module.cloud_aws.cluster_version
+  value       = module.cloud_aws.0.cluster_version
 }
 
 output "cluster_certificate_authority_data" {
   description = "Base64 encoded certificate data required to communicate with your cluster."
-  value       = module.cloud_aws.cluster_certificate_authority_data
+  value       = module.cloud_aws.0.cluster_certificate_authority_data
 }
 
 output "kubeconfig" {
   description = "kubectl config file contents for this EKS cluster."
-  value       = module.cloud_aws.kubeconfig
+  value       = module.cloud_aws.0.kubeconfig
 }

2e15cecb28f524a359fe3ec2166df0693305b440
Andy Bettisworth <andy.bettisworth@gmail.com>
2021-10-07 01:57:37 -0500

    Ignore .kube config files

--- modules/cloud-aws/kubectl.tf
+++ modules/cloud-aws/kubectl.tf
@@ -3,7 +3,7 @@
 # https://kubernetes.io/docs/concepts/configuration/organize-cluster-access-kubeconfig
 resource "local_file" "kubeconfig" {
   content              = local.kubeconfig
-  filename             = "~/.kube/kubeconfig_${local.cluster_name}"
+  filename             = ".kube/kubeconfig_${local.cluster_name}"
   file_permission      = "0600"
   directory_permission = "0755"
 }
